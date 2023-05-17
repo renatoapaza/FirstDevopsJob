6c306c73cfbe5b8fba5060fdd00fbc102068ff64
Renato Apaza <renato.apaza@outlook.com>
2023-05-17 06:05:13 -0400

    add fecha

--- src/main/java/aforo255/com/controller/TestController.java
+++ src/main/java/aforo255/com/controller/TestController.java
@@ -3,11 +3,13 @@ package aforo255.com.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class TestController {
 
 	@GetMapping("/test")
 	public String TestFirstJob() {
-		return "Welcome to MasterDevops |Tomcat v1345 | Ansible , AFORO255";
+		return "Welcome to MasterDevops |Tomcat v1345 | Ansible , AFORO255" + new Date();
 	}
 }
